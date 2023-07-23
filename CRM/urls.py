b08719a41a2836a9f5c34f0c9c3edb54ce31fa2c
Matheus Apalosqui <matheusapalosqui@gmail.com>
2023-07-23 14:07:06 -0300

    NavBar defined.

--- CRM/urls.py
+++ CRM/urls.py
@@ -18,5 +18,7 @@ from django.urls import path
 from website import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    # path('login/', views.login_user(), name='login'),
+    # path('logout/', views.logout_user(), name='logout'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
